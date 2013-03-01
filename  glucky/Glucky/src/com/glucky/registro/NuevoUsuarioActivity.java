a908a82a24d88cb4fac0474365a86af3b43d662a
luislics18@gmail.com <luislics18@gmail.com@0a276db8-55eb-a8b6-09e5-7ed295d08dbd>
2013-03-01 17:45:53 +0000

    Splash screen, main y activity del registro

---
+++  glucky/Glucky/src/com/glucky/registro/NuevoUsuarioActivity.java
@@ -0,0 +1,16 @@
+package com.glucky.registro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.glucky.R;
+
+public class NuevoUsuarioActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.nuevo_user);
+				
+	
+	}
+
+}

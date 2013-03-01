eb778e27d720e1930ac9fcc2214e6394dba4d946
luislics18@gmail.com <luislics18@gmail.com@0a276db8-55eb-a8b6-09e5-7ed295d08dbd>
2013-03-01 17:45:53 +0000

    Splash screen, main y activity del registro

--- trunk/ glucky/Glucky/src/com/example/glucky/MainActivity.java
+++ trunk/ glucky/Glucky/src/com/example/glucky/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.glucky;
 
+import com.glucky.registro.NuevoUsuarioActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView nuevo_usuario;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		nuevo_usuario=(TextView)findViewById(R.id.BtnNvoUser);
+		nuevo_usuario.setVisibility(View.VISIBLE);
+		nuevo_usuario.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(new Intent(MainActivity.this, NuevoUsuarioActivity.class)));
+			}
+		});
 	}
 
 	@Override

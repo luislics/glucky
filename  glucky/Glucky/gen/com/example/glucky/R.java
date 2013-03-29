5eda2136f5743206bea7927e921183b5465fec02
luislics18@gmail.com <luislics18@gmail.com@0a276db8-55eb-a8b6-09e5-7ed295d08dbd>
2013-03-29 05:13:05 +0000

    Te pone texto en la grafica y una carita feliz o triste

---  glucky/Glucky/gen/com/example/glucky/R.java
+++  glucky/Glucky/gen/com/example/glucky/R.java
@@ -21,15 +21,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int happy=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int sad=0x7f020003;
     }
     public static final class id {
         public static final int BtnNvoUser=0x7f08000d;
-        public static final int action_settings=0x7f080018;
+        public static final int action_settings=0x7f08001a;
         public static final int botonlogin=0x7f08000b;
         public static final int boxusuario=0x7f08000a;
-        public static final int btnCrear=0x7f080014;
+        public static final int btnCrear=0x7f080016;
         public static final int btnGuardar=0x7f080004;
         public static final int btnGustos=0x7f080005;
         public static final int button1=0x7f080010;
@@ -37,18 +39,20 @@ public final class R {
         public static final int etClave=0x7f080009;
         public static final int etEdad=0x7f080000;
         public static final int etEstatura=0x7f080002;
-        public static final int etPas=0x7f080013;
+        public static final int etPas=0x7f080015;
         public static final int etPeso=0x7f080001;
-        public static final int etUs=0x7f080012;
+        public static final int etUs=0x7f080014;
         public static final int etUsuario=0x7f080008;
         public static final int glucographic=0x7f080011;
-        public static final int imgSintoma=0x7f080015;
-        public static final int label=0x7f080017;
+        public static final int imagengrafica=0x7f080012;
+        public static final int imgSintoma=0x7f080017;
+        public static final int label=0x7f080019;
         public static final int lvGustos=0x7f080006;
-        public static final int rate=0x7f080016;
+        public static final int rate=0x7f080018;
         public static final int spSexo=0x7f080003;
         public static final int textView1=0x7f080007;
         public static final int textView2=0x7f08000c;
+        public static final int titulofeed=0x7f080013;
         public static final int tvBienvenida=0x7f08000e;
     }
     public static final class layout {
---  glucky/Glucky/src/com/example/glucky/GraficadorGlucosa.java
+++  glucky/Glucky/src/com/example/glucky/GraficadorGlucosa.java
@@ -7,8 +7,11 @@ import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.LineGraphView;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class GraficadorGlucosa extends Activity {
 
@@ -16,27 +19,43 @@ public class GraficadorGlucosa extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.glucosa);
+		double x1=2.0;
+		double x2= 1.5;
+		double x3 =2.5;
+		double x4=1.0;
+		double suma =x1+x2+x3+x4;
+		double resglucosa=suma/4;
 		// graph with dynamically genereated horizontal and vertical labels
 		 GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {  
-	              new GraphViewData(1, 2.0d)  
-	              , new GraphViewData(2, 1.5d)  
-	              , new GraphViewData(3, 2.5d)  
-	              , new GraphViewData(4, 1.0d)  
+	              new GraphViewData(1, x1)  
+	              , new GraphViewData(2, x2)  
+	              , new GraphViewData(3, x3)  
+	              , new GraphViewData(4, x4)  
 	        });  
 
 
 	        GraphView graphView = new BarGraphView(  
 	              this // context  
-	              , "GraphViewDemo" // heading  
+	              , "Tus Niveles de glucosa" // heading  
 	        );  
 	        graphView.addSeries(exampleSeries); // data  
 	        graphView.setHorizontalLabels(new String[]{"Antier","Ayer","Hoy"});
 	        graphView.setVerticalLabels(new String[]{"Bajo","Medio","Alto"});
 	        graphView.setViewPort(2, 40);
-	        graphView.setScrollable(true);
-	        graphView.setScalable(true);
-	        LinearLayout layout = (LinearLayout) findViewById(R.id.glucographic);  
+	       // graphView.setScrollable(true);
+	        //graphView.setScalable(true);
+	        LinearLayout layout = (LinearLayout) findViewById(R.id.glucographic);
+	        ImageView buenaomala=(ImageView)findViewById(R.id.imagengrafica);
+	        TextView nivel= (TextView)findViewById(R.id.textView1);
 	        layout.addView(graphView);
+	        if (resglucosa>1){
+		        buenaomala.setImageResource(R.drawable.sad);
+		        nivel.setText("Tu nivel actual es malo");
+	        }else{
+	        buenaomala.setImageResource(R.drawable.happy);
+	        nivel.setText("Tu nivel actual es bueno");
+	        }
+	        nivel.setTextColor(Color.WHITE);
 	}
 
 }
